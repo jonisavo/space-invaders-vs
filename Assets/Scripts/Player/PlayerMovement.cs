719e0ac6f9fa857b4d2967bad3aac12708a284ba
Joni Savolainen <joni@savolainen.io>
2020-12-01 18:10:31 +0200

    Add Scripts/Player/ folder

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 10;
+    public float RotateSpeed = 250;
+
+    private void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime,
+            0,
+            Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime);
+
+        transform.Rotate(
+            new Vector3(0, Input.GetAxis("Mouse X") * RotateSpeed * Time.deltaTime, 0));
+    }
+}
