3f63b6b9b80510e6344336410011bac142301386
Joni Savolainen <joni@savolainen.io>
2020-12-01 17:52:40 +0200

    Add Scripts/UI/ folder

---
+++ Assets/Scripts/UI/PlayerNameInputField.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+using System.Collections;
+
+namespace SIVS
+{
+    /// <summary>
+    /// Player name input field. Let the user input his name, will appear above the player in the game.
+    /// </summary>
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        private const string playerNamePrefKey = "PlayerName";
+
+        private void Start()
+        {
+            string defaultName = string.Empty;
+            InputField inputField = this.GetComponent<InputField>();
+            if (inputField != null)
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    inputField.text = defaultName;
+                }
+
+            PhotonNetwork.NickName = defaultName;
+        }
+
+        public void SetPlayerValue(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Player name is null or empty.");
+                return;
+            }
+
+            PhotonNetwork.NickName = value;
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+    }
+}
