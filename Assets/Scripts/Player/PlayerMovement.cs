30621029ca44986953a0f0f672bd193951ac27c2
Joni Savolainen <joni@savolainen.io>
2020-12-08 17:25:39 +0200

    Add Rigidbody2D to player ship

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 namespace SIVS
 {
@@ -6,12 +7,20 @@ namespace SIVS
     {
         public float MoveSpeed = 10;
 
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
         private void Update()
         {
-            transform.Translate(
-                Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime,
-                0,
-                0);
+            _rb.MovePosition((Vector2)transform.position + Vector2.right * Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime);
+            //transform.Translate(
+            //    Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime,
+            //    0,
+            //    0);
         }
     }
 }
