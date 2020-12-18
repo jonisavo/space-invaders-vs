09e9b17fa973e452b4af098a990c9cd7c03b1742
Joni Savolainen <joni@savolainen.io>
2020-12-18 11:32:26 +0200

    Add PlayerHealth.cs

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace SIVS
+{
+    public class PlayerHealth : MonoBehaviourPunCallbacks
+    {
+        private int Lives = 3;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!photonView.IsMine) return;
+
+            if (!other.gameObject.CompareTag("EnemyBullet")) return;
+            
+            GetHit();
+        }
+        
+        private void GetHit()
+        {
+            photonView.RPC("LoseLife", RpcTarget.AllBuffered);
+        }
+
+        [PunRPC]
+        private void LoseLife()
+        {
+            Lives--;
+        }
+    }
+}
