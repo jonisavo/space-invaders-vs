b41a38da1fa2d41858add939f28441a3c0c8daf5
Joni Savolainen <joni@savolainen.io>
2020-12-19 19:31:26 +0200

    Add GameRandomizer.cs

---
+++ Assets/Scripts/Game/GameRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Random = System.Random;
+
+namespace SIVS
+{
+    public class GameRandomizer : MonoBehaviour
+    {
+        private Random rand;
+
+        private void Awake()
+        {
+            rand = new Random(PhotonNetwork.CurrentRoom.GetHashCode());
+        }
+
+        public int GetInt(int minValue, int maxValue)
+        {
+            return rand.Next(minValue, maxValue);
+        }
+    }
+}
+
