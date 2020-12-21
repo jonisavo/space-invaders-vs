1be2789fa490974cac3b8afc1a422364b7b0d096
Joni Savolainen <joni@savolainen.io>
2020-12-21 21:20:34 +0200

    Add heart sprite and UIManager script to display lives

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public class UIManager : MonoBehaviour
+    {
+        public Texture lifeTexture;
+
+        private GameStatistics _statistics;
+
+        private void Start()
+        {
+            _statistics = GameObject.Find("Game Manager").GetComponent<GameStatistics>();
+        }
+
+        private void OnGUI()
+        {
+            DrawLives();
+        }
+
+        private void DrawLives()
+        {
+            for (int i = 0; i < _statistics.GetOwnStatistics().Lives; i++)
+                GUI.DrawTexture(new Rect(12 + 40 + 40 * i, 656, 32, 32), lifeTexture);
+
+            for (int i = 0; i < _statistics.GetOpponentStatistics().Lives; i++)
+                GUI.DrawTexture(new Rect(1024 - 36 - 40 - 40 * i, 656, 32, 32), lifeTexture);
+        }
+    }
+}
