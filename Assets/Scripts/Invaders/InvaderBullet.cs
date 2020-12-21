5da003c1ee8ceb53167ae0e007d0aa0cfecfe596
Joni Savolainen <joni@savolainen.io>
2020-12-21 20:08:54 +0200

    Add invader bullet prefab and script

---
+++ Assets/Scripts/Invaders/InvaderBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public class InvaderBullet : MonoBehaviour
+    {
+        private const float DespawnHeight = -2.4f;
+
+        public float moveSpeed = 1.0f;
+
+        private void Update()
+        {
+            transform.Translate(
+                0,
+                -moveSpeed * Time.deltaTime,
+                0);
+            if (transform.position.y < DespawnHeight)
+                Destroy(gameObject);
+        }
+    }
+}
