23eee4842885cbfe67d4f2c6a8fd2cc9e17dd760
Joni Savolainen <joni@savolainen.io>
2020-12-23 09:21:18 +0200

    Initial (scuffed) victory window and correct drawing of life sprites

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
 
 namespace SIVS
 {
     public class UIManager : MonoBehaviour
     {
         public Texture lifeTexture;
+
+        public GameObject resultCanvas;
+
+        public TMP_Text victoryText;
 
         private GameStatistics _statistics;
 
@@ -18,13 +24,21 @@ namespace SIVS
             DrawLives();
         }
 
+        public void ShowVictoryScreen(string nickName)
+        {
+            resultCanvas.SetActive(true);
+            victoryText.text = nickName + " won!";
+        }
+
         private void DrawLives()
         {
-            for (var i = 0; i < _statistics.GetOwnStatistics().Lives; i++)
-                GUI.DrawTexture(new Rect(12 + 40 + 40 * i, 656, 32, 32), lifeTexture);
-
-            for (var i = 0; i < _statistics.GetOpponentStatistics().Lives; i++)
-                GUI.DrawTexture(new Rect(1024 - 36 - 40 - 40 * i, 656, 32, 32), lifeTexture);
+            foreach (var entry in PhotonNetwork.CurrentRoom.Players)
+            {
+                var initialXCoord = entry.Key % 2 == 0 ? 948 : 52;
+                for (var i = 0; i < _statistics.GetStatistics(entry.Value.NickName).Lives; i++)
+                    GUI.DrawTexture(new Rect(entry.Key % 2 == 0 ? initialXCoord - 40 * i : initialXCoord + 40 * i, 
+                            656, 32, 32), lifeTexture);
+            }
         }
     }
 }
