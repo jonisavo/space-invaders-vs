08685f40be644a085218bbb767904ae38683028b
Joni Savolainen <joni@savolainen.io>
2020-12-26 16:00:29 +0200

    Add JoinRoomButton.cs

---
+++ Assets/Scripts/UI/JoinRoomButton.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class JoinRoomButton : MonoBehaviour
+    {
+        public TMP_InputField roomNameInputField;
+
+        public Matchmaker matchmaker;
+
+        public void JoinRoom()
+        {
+            var roomName = roomNameInputField.text.Trim();
+            
+            if (string.IsNullOrEmpty(roomName) || string.IsNullOrEmpty(PhotonNetwork.NickName.Trim()))
+                return;
+
+            matchmaker.JoinNamedRoom(roomName);
+        }
+    }
+}
