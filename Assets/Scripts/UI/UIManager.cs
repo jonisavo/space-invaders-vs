ddf878cc401e6318af06a6b8116f2cdcc0a3a820
Joni Savolainen <joni@savolainen.io>
2020-12-30 15:39:50 +0200

    Draw players' points on screen

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine;
@@ -18,18 +19,29 @@ namespace SIVS
         [Tooltip("The text to write the result of a game on.")]
         public TMP_Text victoryText;
 
+        [Tooltip("GUI style to write UI text with.")]
+        public GUIStyle guiStyle;
+
         private Dictionary<string, int> _cachedLives;
+
+        private Dictionary<string, int> _cachedPoints;
+
+        private GUIStyle _guiStyle;
         
         #region MonoBehaviour Callbacks
 
         private void Awake()
         {
             _cachedLives = new Dictionary<string, int>();
+            _cachedPoints = new Dictionary<string, int>();
         }
 
         private void OnGUI()
         {
+            if (!PhotonNetwork.InRoom) return;
+            
             DrawLives();
+            DrawPoints();
         }
         
         #endregion
@@ -52,8 +64,6 @@ namespace SIVS
 
         private void DrawLives()
         {
-            if (!PhotonNetwork.InRoom) return;
-            
             foreach (var entry in PhotonNetwork.CurrentRoom.Players)
             {
                 if (!_cachedLives.ContainsKey(entry.Value.NickName)) continue;
@@ -64,6 +74,13 @@ namespace SIVS
                     GUI.DrawTexture(new Rect(entry.Key % 2 == 0 ? initialXCoord - 40 * i : initialXCoord + 40 * i, 
                             656, 32, 32), lifeTexture);
             }
+        }
+
+        private void DrawPoints()
+        {
+            foreach (var entry in PhotonNetwork.CurrentRoom.Players)
+                GUI.Label(new Rect(entry.Key == 1 ? 52 : 914, 700, 256, 64),
+                    entry.Value.GetScore().ToString("D5"), guiStyle);
         }
     }
 }
