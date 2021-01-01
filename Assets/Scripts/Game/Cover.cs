e223a42d4d144430c49be34ef565eff9c4009c2b
Joni Savolainen <joni@savolainen.io>
2021-01-01 19:28:35 +0200

    Implement cover

---
+++ Assets/Scripts/Game/Cover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class Cover : MonoBehaviourPunCallbacks
+    {
+        [Tooltip("The cover piece object the cover consists of.")]
+        public GameObject coverPiece;
+        
+        [Tooltip("The number of cover piece rows.")]
+        public int rows = 5;
+
+        [Tooltip("The number of cover piece columns.")]
+        public int columns = 4;
+        
+        private Dictionary<int, GameObject> _pieces;
+
+        private void Awake()
+        {
+            _pieces = new Dictionary<int, GameObject>();
+        }
+
+        private void Start()
+        {
+            InstantiatePieces();
+        }
+
+        [PunRPC]
+        public void DestroyPiece(int id)
+        {
+            if (!_pieces.ContainsKey(id)) return;
+
+            Destroy(_pieces[id]);
+
+            _pieces.Remove(id);
+        }
+        
+        public void InstantiatePieces()
+        {
+            var id = 0;
+
+            for (var row = 0; row < rows; row++)
+            {
+                for (var column = 0; column < columns; column++)
+                {
+                    var piece = Instantiate(coverPiece, new Vector3(row * 0.08f, column * 0.08f, 0),
+                        Quaternion.identity);
+                    
+                    piece.transform.SetParent(gameObject.transform, false);
+
+                    piece.GetComponent<CoverPiece>().InitializeCoverPiece(id, this);
+
+                    _pieces[id++] = piece;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/CoverPiece.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class CoverPiece : MonoBehaviour
+    {
+        private Cover _cover;
+
+        private int _id;
+        
+        #region MonoBehaviour Callbacks
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("PlayerBullet") && !other.gameObject.CompareTag("EnemyBullet"))
+                return;
+            
+            Destroy(other.gameObject);
+
+            if (!_cover.photonView.IsMine) return;
+
+            _cover.photonView.RPC("DestroyPiece", RpcTarget.AllBuffered, _id);
+        }
+        
+        #endregion
+
+        public void InitializeCoverPiece(int id, Cover coverComponent)
+        {
+            _id = id;
+            _cover = coverComponent;
+        }
+    }
+}
