7446c8c8878dc873f9a2b88bdf71f72ad874d54c
Joni Savolainen <joni@savolainen.io>
2021-01-04 20:01:44 +0200

    Add UFOHealth component

---
+++ Assets/Scripts/Invaders/UFOHealth.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class UFOHealth : MonoBehaviourPunCallbacks
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("PlayerBullet")) return;
+            
+            Destroy(other.gameObject);
+            
+            if (!photonView.IsMine) return;
+            
+            var bulletOwner = other.gameObject.GetComponent<PlayerBullet>().Owner;
+            
+            bulletOwner.AddScore(1000);
+            
+            photonView.RPC("Die", RpcTarget.All);
+        }
+
+        [PunRPC]
+        private void Die()
+        {
+            if (photonView.IsMine)
+                PhotonNetwork.Destroy(gameObject);
+            else
+                if (gameObject) gameObject.SetActive(false);
+        }
+    }
+}
