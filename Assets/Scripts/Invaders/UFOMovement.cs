28c37bc0022d182548d0afb94770b33219c94811
Joni Savolainen <joni@savolainen.io>
2021-01-04 19:47:08 +0200

    Add UFOMovement component

---
+++ Assets/Scripts/Invaders/UFOMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public class UFOMovement : MonoBehaviour
+    {
+        [Tooltip("The movement speed of the UFO.")]
+        public float moveSpeed;
+
+        private Vector2 _movementDirection;
+
+        #region MonoBehaviour Callbacks
+        
+        private void Awake()
+        {
+            _movementDirection = Vector2.zero;
+        }
+
+        private void Update()
+        {
+            transform.Translate(_movementDirection * (moveSpeed * Time.deltaTime));
+        }
+        
+        #endregion
+
+        public void SetMovementDirection(Vector2 direction) => _movementDirection = direction;
+    }
+}
