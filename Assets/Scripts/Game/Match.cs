53f1efddbbb978d35b38c8273d36a3c5b4441ee9
Joni Savolainen <joni@savolainen.io>
2021-01-04 16:38:32 +0200

    Add Match.cs

---
+++ Assets/Scripts/Game/Match.cs
@@ -0,0 +1,26 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+
+namespace SIVS
+{
+    public static class Match
+    {
+        public static bool IsActive
+        {
+            set
+            {
+                PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable()
+                {
+                    {"Active", value}
+                });
+            }
+            get
+            {
+                if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("Active", out var isActive))
+                    return (bool) isActive;
+
+                return false;
+            }
+        }
+    }
+}
