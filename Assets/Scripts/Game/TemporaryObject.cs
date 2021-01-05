addd717f5d1a7c70182353d83ae2b8d45c565554
Joni Savolainen <joni@savolainen.io>
2021-01-05 19:19:02 +0200

    Add TemporaryObject.cs

---
+++ Assets/Scripts/Game/TemporaryObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public class TemporaryObject : MonoBehaviour
+    {
+        [Tooltip("Time in seconds until the object is destroyed.")]
+        public float waitTime = 3.0f;
+
+        private void Start()
+        {
+            Destroy(gameObject, waitTime);
+        }
+    }
+}
