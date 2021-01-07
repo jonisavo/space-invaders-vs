89190e2374b7c2b0da1ae2412033b7dd9fd6727e
Joni Savolainen <joni@savolainen.io>
2021-01-07 16:19:10 +0200

    Play endgame music on Round 5

---
+++ Assets/Scripts/Audio/RoundMusic.cs
@@ -0,0 +1,41 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(MusicLoop))]
+    public class RoundMusic : MonoBehaviourPunCallbacks
+    {
+        public int round = 1;
+
+        public AudioClip intro;
+
+        public AudioClip loop;
+
+        private bool _reachedRound = false;
+
+        private MusicLoop _musicLoop;
+
+        private void Awake()
+        {
+            _musicLoop = GetComponent<MusicLoop>();
+        }
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+        {
+            if (!changedProps.ContainsKey(PlayerStats.CurrentRound))
+                return;
+            
+            if (!_reachedRound && (int) changedProps[PlayerStats.CurrentRound] >= round)
+                PlayMusic();
+        }
+
+        private void PlayMusic()
+        {
+            _musicLoop.Play(intro, loop);
+            _reachedRound = true;
+        }
+    }
+}
