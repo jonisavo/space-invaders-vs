b4ce8c4aa10f093e67b23b2e6eae891b865777aa
Joni Savolainen <joni@savolainen.io>
2021-01-07 16:56:43 +0200

    Add volume options to the main menu

---
+++ Assets/Scripts/UI/SliderConfirmSound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(Slider))]
+    [RequireComponent(typeof(AudioSource))]
+    public class SliderConfirmSound : MonoBehaviour, IPointerUpHandler
+    {
+        private Slider _slider;
+
+        private AudioSource _audioSource;
+
+        private float _oldValue;
+
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_slider.value != _oldValue)
+                _audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(Slider))]
+    public class VolumeSlider : MonoBehaviour
+    {
+        public AudioMixer mixer;
+
+        public string parameterName;
+
+        private void Awake()
+        {
+            GetComponent<Slider>().value = 
+                PlayerPrefs.GetFloat(parameterName, 0.8f);
+        }
+
+        public void SetVolume(float sliderValue)
+        {
+            ChangeVolume(sliderValue);
+            PlayerPrefs.SetFloat(parameterName, sliderValue);
+            Debug.Log($"Setting {parameterName} to {Mathf.Log10(sliderValue) * 20}");
+        }
+
+        private void ChangeVolume(float sliderValue) =>
+            mixer.SetFloat(parameterName, Mathf.Log10(sliderValue) * 20);
+    }
+}
