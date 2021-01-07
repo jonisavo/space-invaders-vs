48cb7ffb0824884285b4d9b2af7978e1c511f0ad
Joni Savolainen <joni@savolainen.io>
2021-01-07 19:45:35 +0200

    Add SoundPlayer static class

---
+++ Assets/Scripts/Audio/SoundPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public static class SoundPlayer
+    {
+        public static void PlaySound(AudioClip clip, float volumeScale = 1.0f)
+        {
+            GameObject.Find("Sound Player")
+                .GetComponent<AudioSource>()
+                .PlayOneShot(clip, volumeScale);
+        }
+    }
+}
