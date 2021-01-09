6efd4215cdc314b63f958273c7968d035070aa80
Joni Savolainen <joni@savolainen.io>
2021-01-09 15:03:35 +0200

    Remove angular drag from player ships

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,7 +18,7 @@ namespace SIVS
 
         private void FixedUpdate()
         {
-            if (!photonView.IsMine || !Match.IsActive) 
+            if (!photonView.IsMine || !Match.IsActive)
                 return;
 
             _rb.MovePosition((Vector2)transform.position + Vector2.right * (Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime));
