d7c28d443e07107998c29d59794a846c9a6be5d2
Joni Savolainen <joni@savolainen.io>
2021-01-09 15:09:57 +0200

    Add version number to the main menu

---
+++ Assets/Scripts/UI/VersionNumberText.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class VersionNumberText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TMP_Text>().text = Application.version;
+        }
+    }
+}
