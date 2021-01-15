9fe5d99da5c25dc315b44d27bd63f33e1c30f68f
Joni Savolainen <joni@savolainen.io>
2021-01-15 14:09:35 +0200

    Cache animator parameter id

--- Assets/Scripts/Invaders/InvaderSprite.cs
+++ Assets/Scripts/Invaders/InvaderSprite.cs
@@ -9,10 +9,12 @@ namespace SIVS
         [Tooltip("Specifies the number of sprite types to cycle through.")]
         public int typeCount;
 
+        private static readonly int Type = Animator.StringToHash("Type");
+
         private void Awake()
         {
             var rand = new Random(System.Guid.NewGuid().GetHashCode());
-            GetComponent<Animator>().SetInteger("Type", rand.Next(typeCount));
+            GetComponent<Animator>().SetInteger(Type, rand.Next(typeCount));
         }
     }
 }
