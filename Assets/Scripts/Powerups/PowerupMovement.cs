beca1f0a0ddb2b43b9e08dbf5cb5cdafb54392e6
Joni Savolainen <joni@savolainen.io>
2021-01-15 14:06:10 +0200

    Rename FallSpeed field to fallSpeed

--- Assets/Scripts/Powerups/PowerupMovement.cs
+++ Assets/Scripts/Powerups/PowerupMovement.cs
@@ -8,11 +8,11 @@ namespace SIVS
         private const float DespawnHeight = -2.2f;
 
         [Tooltip("Determines how fast the powerup falls.")]
-        public float FallSpeed = 1.0f;
+        public float fallSpeed = 1.0f;
 
         private void Update()
         {
-            transform.Translate(Vector3.down * (FallSpeed * Time.deltaTime));
+            transform.Translate(Vector3.down * (fallSpeed * Time.deltaTime));
 
             if (transform.position.y < DespawnHeight)
                 DestroyPowerup();
