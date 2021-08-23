8273fa563981f9f330f5a380b79e7894b11dd16d
Joni Savolainen <joni@savolainen.io>
2021-08-23 18:05:14 +0300

    Add unity-text-typer

---
+++ Assets/Scripts/UI/TextPopup.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using RedBlueGames.Tools.TextTyper;
+using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(TextTyper))]
+    public class TextPopup : MonoBehaviour
+    {
+        [TextArea]
+        public string text;
+
+        [Range(0.5f, 10f)]
+        public float duration = 1.0f;
+
+        [Range(0, 2f)]
+        public float typeDelay = 0.02f;
+
+        public float untypeDelay = -1;
+
+        public bool activateAutomatically;
+
+        public delegate void SpawnDelegate(TextPopup textPopup);
+
+        public static event SpawnDelegate OnSpawn;
+
+        private TextTyper _textTyper;
+
+        private void Awake()
+        {
+            _textTyper = GetComponent<TextTyper>();
+            OnSpawn?.Invoke(this);
+        }
+
+        private void OnEnable()
+        {
+            _textTyper.PrintCompleted.AddListener(() => StartCoroutine(nameof(Hide)));
+
+            if (activateAutomatically)
+                Show();
+        }
+
+        private void OnDisable()
+        {
+            _textTyper.PrintCompleted.RemoveListener(() => StartCoroutine(nameof(Hide)));
+        }
+
+        public void Show()
+        {
+            _textTyper.TypeText(text, typeDelay);
+        }
+
+        public void Show(string textToShow)
+        {
+            _textTyper.TypeText(textToShow, typeDelay);
+        }
+
+        public void ChangeText(string newText)
+        {
+            text = newText;
+        }
+
+        private IEnumerator Hide()
+        {
+            yield return new WaitForSeconds(duration);
+            
+            yield return _textTyper.UntypeTextCharByChar(untypeDelay);
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TextPopupCanvas.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(Canvas))]
+    public class TextPopupCanvas : MonoBehaviour
+    {
+        private void OnEnable() => TextPopup.OnSpawn += OnTextPopupSpawn;
+
+        private void OnDisable() => TextPopup.OnSpawn -= OnTextPopupSpawn;
+
+        private void OnTextPopupSpawn(TextPopup textPopup)
+        {
+            var popupTransform = textPopup.transform;
+            
+            popupTransform.SetParent(transform, true);
+
+            var position = popupTransform.position;
+
+            popupTransform.position = new Vector3(position.x, position.y, -2f);
+        }
+    }
+}
