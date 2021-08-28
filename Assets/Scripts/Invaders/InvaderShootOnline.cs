6891cd448482965fe2753a5421554e4ea877bdf4
Joni Savolainen <joni@savolainen.io>
2021-08-28 20:04:51 +0300

    Local multiplayer: rework invader shooting

---
+++ Assets/Scripts/Invaders/InvaderShootOnline.cs
@@ -0,0 +1,47 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(PhotonView))]
+    public class InvaderShootOnline : InvaderShoot
+    {
+        private PhotonView _photonView;
+        
+        protected override void Awake()
+        {
+            _photonView = GetComponent<PhotonView>();
+            base.Awake();
+        }
+        
+        protected override void StartShooting()
+        {
+            if (!_photonView.IsMine)
+                return;
+            
+            base.StartShooting();
+        }
+
+        public override void StopShooting()
+        {
+            if (!_photonView.IsMine)
+                return;
+            
+            base.StopShooting();
+        }
+        
+        protected override void Shoot() =>
+            _photonView.RPC(nameof(ShootRPC), RpcTarget.All);
+
+        [PunRPC]
+        private void ShootRPC() => base.Shoot();
+
+        protected override void PlayShootSound()
+        {
+            if (!_photonView.IsMine)
+                return;
+            
+            base.PlayShootSound();
+        }
+    }
+}
