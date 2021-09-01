b7662e57ce743b96ac154b35e59ba9ba5cf79e2c
Joni Savolainen <joni@savolainen.io>
2021-09-01 17:21:08 +0300

    Add MaterialOffsetScroll component

---
+++ Assets/Scripts/General/MaterialOffsetScroll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(Renderer))]
+    public class MaterialOffsetScroll : MonoBehaviour
+    {
+        [Range(-1f, 1f)]
+        public float xScrollSpeed;
+
+        [Range(-1f, 1f)]
+        public float yScrollSpeed;
+
+        private Renderer _renderer;
+        
+        private static readonly int mainTex = Shader.PropertyToID("_MainTex");
+
+        private void Awake() => _renderer = GetComponent<Renderer>();
+
+        private float _xScroll;
+
+        private float _yScroll;
+
+        private void FixedUpdate()
+        {
+            var x = Mathf.Repeat(_xScroll += xScrollSpeed * Time.fixedDeltaTime, 1);
+            var y = Mathf.Repeat(_yScroll += yScrollSpeed * Time.fixedDeltaTime, 1);
+            _renderer.sharedMaterial.SetTextureOffset(mainTex, new Vector2(x, y));
+        }
+    }
+}
