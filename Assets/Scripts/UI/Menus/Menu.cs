f75e8df7a51d555e78b2e9bc35f06e96c6d4b6ad
Joni Savolainen <joni@savolainen.io>
2021-09-01 17:20:32 +0300

    Add initial Menu and MenuManager scripts

---
+++ Assets/Scripts/UI/Menus/Menu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Menu : MonoBehaviour
+    {
+        [Tooltip("A Selectable UI component to select automatically when showing this menu.")]
+        public Selectable autoSelect;
+        
+        [Tooltip("Disables going back to the previous menu via the cancel button.")]
+        public bool disableGoingBack;
+        
+        private CanvasGroup _canvasGroup;
+
+        private void Awake() => _canvasGroup = GetComponent<CanvasGroup>();
+
+        public void Show()
+        {
+            _canvasGroup.interactable = true;
+            _canvasGroup.alpha = 1.0f;
+            _canvasGroup.blocksRaycasts = true;
+            
+            if (autoSelect)
+                autoSelect.Select();
+        }
+
+        public void Hide()
+        {
+            _canvasGroup.interactable = false;
+            _canvasGroup.alpha = 0.0f;
+            _canvasGroup.blocksRaycasts = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menus/MenuManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [Min(0)]
+        public int id;
+        
+        public SerializableDictionary<string, Menu> menus;
+
+        public string defaultMenu;
+
+        private readonly Stack<Menu> _history = new Stack<Menu>();
+
+        private delegate void PushMenuDelegate(string menuName, int managerId = 0);
+
+        private static event PushMenuDelegate OnPushMenu;
+
+        public static void PushMenu(string menuName, int managerId = 0) =>
+            OnPushMenu?.Invoke(menuName, managerId);
+
+        private void OnEnable() => OnPushMenu += HandlePushMenu;
+
+        private void OnDisable() => OnPushMenu -= HandlePushMenu;
+        
+        private void Awake()
+        {
+            if (!string.IsNullOrEmpty(defaultMenu))
+                Push(defaultMenu);
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Cancel"))
+                GoBack();
+        }
+
+        public void Push(string menuName)
+        {
+            if (!menus.ContainsKey(menuName))
+            {
+                Debug.LogError("No menu named " + menuName + " found in MenuManager", gameObject);
+                return;
+            }
+            
+            if (_history.Count > 0)
+                _history.Peek().Hide();
+            
+            _history.Push(menus[menuName]);
+            
+            _history.Peek().Show();
+        }
+
+        public void Pop()
+        {
+            if (_history.Count == 0)
+                return;
+            
+            _history.Pop().Hide();
+            
+            _history.Peek().Show();
+        }
+
+        public void GoBack()
+        {
+            if (_history.Count <= 1 || _history.Peek().disableGoingBack)
+                return;
+
+            Pop();
+        }
+
+        private void HandlePushMenu(string menuName, int managerId = 0)
+        {
+            if (managerId != id)
+                return;
+            
+            Push(menuName);
+        }
+    }
+}
