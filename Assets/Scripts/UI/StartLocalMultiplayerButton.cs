7ab937e1143e2436e8a3b7a872056b4ec92ad32b
Joni Savolainen <joni@savolainen.io>
2021-09-01 19:36:39 +0300

    Local multiplayer: Allow playing from the main menu

---
+++ Assets/Scripts/UI/StartLocalMultiplayerButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SIVS
+{
+    public class StartLocalMultiplayerButton : MonoBehaviour
+    {
+        public void Press() => SceneManager.LoadScene("InGameOffline");
+    }
+}
