41cdfc2c461463d1077fa1bb7d0474ce03501161
Joni Savolainen <joni@savolainen.io>
2021-09-05 11:48:13 +0300

    Add main menu toggle for fullscreen

---
+++ Assets/Scripts/UI/Toggles/FullscreenToggle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    public class FullscreenToggle : MainMenuToggle
+    {
+        protected override bool GetInitialValue()
+        {
+            var fallbackValue = Screen.fullScreen ? 1 : 0;
+
+            return PlayerPrefs.GetInt(PlayerPrefsKeys.ScreenFullscreen, fallbackValue) == 1;
+        }
+
+        protected override void HandleValueChange(bool value)
+        {
+            base.HandleValueChange(value);
+            
+            PlayerPrefs.SetInt(PlayerPrefsKeys.ScreenFullscreen, value ? 1 : 0);
+
+            Screen.fullScreenMode = value ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+            Screen.fullScreen = value;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Toggles/MainMenuToggle.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using RedBlueGames.NotNull;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SIVS
+{
+    [RequireComponent(typeof(Toggle))]
+    public class MainMenuToggle : MainMenuSelectable
+    {
+        [Header("Toggle")]
+        [NotNull]
+        public TMP_Text onLabel;
+
+        [NotNull]
+        public TMP_Text offLabel;
+        
+        public Color enabledColor = Color.white;
+
+        public Color disabledColor = Color.gray;
+
+        private Toggle _toggle;
+
+        private Coroutine _horizontalInputCoroutine;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            _toggle = GetComponent<Toggle>();
+            
+            _toggle.onValueChanged.AddListener(HandleValueChange);
+
+            _toggle.SetIsOnWithoutNotify(GetInitialValue());
+            
+            HandleValueChange(_toggle.isOn);
+        }
+        
+        protected virtual bool GetInitialValue() => _toggle.isOn;
+
+        protected virtual void HandleValueChange(bool value)
+        {
+            if (value)
+            {
+                onLabel.color = enabledColor;
+                offLabel.color = disabledColor;
+            }
+            else
+            {
+                offLabel.color = enabledColor;
+                onLabel.color = disabledColor;
+            }
+        }
+
+        public override void EnableAllAnimation()
+        {
+            base.EnableAllAnimation();
+            
+            if (_horizontalInputCoroutine != null)
+                StopCoroutine(_horizontalInputCoroutine);
+
+            _horizontalInputCoroutine = StartCoroutine(CheckHorizontalInputCoroutine());
+        }
+
+        public override void DisableAllAnimation()
+        {
+            base.DisableAllAnimation();
+
+            if (_horizontalInputCoroutine == null)
+                return;
+            
+            StopCoroutine(_horizontalInputCoroutine);
+
+            _horizontalInputCoroutine = null;
+        }
+
+        private IEnumerator CheckHorizontalInputCoroutine()
+        {
+            var cooldownBetweenToggles = new WaitForSeconds(0.3f);
+            
+            while (true)
+            {
+                yield return null;
+
+                var change = Input.GetAxisRaw("Horizontal") != 0;
+
+                if (!change)
+                    continue;
+                
+                _toggle.isOn = !_toggle.isOn;
+
+                yield return cooldownBetweenToggles;
+            }
+        }
+    }
+}
