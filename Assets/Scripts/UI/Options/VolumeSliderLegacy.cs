ae24db76c487b81d4d269e06c70f4ae8f3a83de5
Joni Savolainen <joni@savolainen.io>
2021-09-05 11:44:24 +0300

    Rename old VolumeSlider.cs to VolumeSliderLegacy.cs

--- Assets/Scripts/UI/Options/VolumeSliderLegacy.cs
+++ Assets/Scripts/UI/Options/VolumeSliderLegacy.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 namespace SIVS
 {
     [RequireComponent(typeof(Slider))]
-    public class VolumeSlider : MonoBehaviour
+    public class VolumeSliderLegacy : MonoBehaviour
     {
         [NotNull]
         public AudioMixer mixer;
