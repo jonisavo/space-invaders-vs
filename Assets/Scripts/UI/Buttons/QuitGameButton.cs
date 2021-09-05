1b58d20ea8b546be13d7f1d816c984e45aebabf1
Joni Savolainen <joni@savolainen.io>
2021-09-05 13:48:37 +0300

    Make the quit game button stop play mode in editor

--- Assets/Scripts/UI/Buttons/QuitGameButton.cs
+++ Assets/Scripts/UI/Buttons/QuitGameButton.cs
@@ -4,6 +4,13 @@ namespace SIVS
 {
     public class QuitGameButton : MonoBehaviour
     {
-        public void Press() => Application.Quit();
+        public void Press()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 }
