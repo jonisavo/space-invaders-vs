32549bf5aa0e16347a9ab248b5f2b7fb0eca938e
Joni Savolainen <joni@savolainen.io>
2021-09-09 11:25:36 +0300

    Start adding additional bling to the main menu

---
+++ Assets/Scripts/General/MoveBackAndForth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class MoveBackAndForth : MonoBehaviour
+    {
+        [Min(0f)]
+        public float distance;
+
+        [Min(0.5f)]
+        public float time;
+
+        private float _startX;
+
+        private float _endX;
+
+        private Coroutine _moveCoroutine;
+
+        private void Awake()
+        {
+            _startX = transform.position.x;
+            _endX = _startX + distance;
+        }
+
+        private void OnEnable()
+        {
+            _moveCoroutine = StartCoroutine(MoveCoroutine());
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(_moveCoroutine);
+            _moveCoroutine = null;
+        }
+
+        private IEnumerator MoveCoroutine()
+        {
+            while (true)
+            {
+                yield return MoveToX(_endX);
+                yield return MoveToX(_startX);
+            }
+        }
+
+        private IEnumerator MoveToX(float targetX)
+        {
+            var elapsedTime = 0f;
+
+            var position = transform.position;
+
+            var newPosition = new Vector3(targetX, position.y, position.z);
+            
+            while (elapsedTime < time)
+            {
+                transform.position = Vector3.Lerp(position, newPosition, elapsedTime / time);
+
+                yield return null;
+
+                elapsedTime += Time.deltaTime;
+            }
+
+            transform.position = newPosition;
+        }
+    }
+}
