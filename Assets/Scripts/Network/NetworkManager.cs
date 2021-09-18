dd8a76c113e701a74389a0f8a7d4f29030601af9
Joni Savolainen <joni@savolainen.io>
2021-09-18 14:34:34 +0300

    Add NetworkManager base class and NetworkManagerPhoton implementation

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace SIVS
+{
+    [DisallowMultipleComponent]
+    public class NetworkManager : MonoBehaviour
+    {
+        public virtual bool IsConnected { get; protected set; }
+        
+        private static NetworkManager _instance;
+
+        public delegate void ConnectDelegate();
+
+        public static event ConnectDelegate OnConnect;
+        
+        public delegate void DisconnectDelegate();
+
+        public static event DisconnectDelegate OnDisconnect;
+
+        private void Awake()
+        {
+            if (_instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
+            _instance = this;
+            
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public virtual bool Connect()
+        {
+            return false;
+        }
+
+        public static bool ConnectUsingCurrentManager()
+        {
+            if (!_instance)
+                throw new UnassignedReferenceException("No NetworkManager is active!");
+
+            return _instance.Connect();
+        }
+
+        public static bool IsConnectedUsingCurrentManager()
+        {
+            if (!_instance)
+                throw new UnassignedReferenceException("No NetworkManager is active!");
+
+            return _instance.IsConnected;
+        }
+
+        protected void EmitConnectedEvent() => OnConnect?.Invoke();
+        
+        protected void EmitDisconnectedEvent() => OnDisconnect?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Network/NetworkManagerPhoton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace SIVS
+{
+    public class NetworkManagerPhoton : NetworkManager, IConnectionCallbacks
+    {
+        public override bool IsConnected => PhotonNetwork.IsConnectedAndReady;
+
+        private const string PhotonGameVersion = "1.2";
+
+        private void OnEnable() => PhotonNetwork.AddCallbackTarget(this);
+
+        private void OnDisable() => PhotonNetwork.RemoveCallbackTarget(this);
+        
+        public override bool Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+                return true;
+            
+            var value = PhotonNetwork.ConnectUsingSettings();
+            
+            PhotonNetwork.GameVersion = PhotonGameVersion;
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.MinimalTimeScaleToDispatchInFixedUpdate = 0;
+            
+            return value;
+        }
+
+        public void OnConnected() { }
+
+        public void OnConnectedToMaster()
+        {
+            EmitConnectedEvent();
+        }
+
+        public void OnDisconnected(DisconnectCause cause)
+        {
+            EmitDisconnectedEvent();
+        }
+
+        public void OnRegionListReceived(RegionHandler _) { }
+
+        public void OnCustomAuthenticationFailed(string _) { }
+
+        public void OnCustomAuthenticationResponse(Dictionary<string, object> _) { }
+    }
+}
